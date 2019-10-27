289c810db42430392e55da8aa1d6d9e62ac203c5
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-27 18:27:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.each_with_index do |names|
+    puts names
+  end
 end
 
 def summon_captain_planet# code an argument here
